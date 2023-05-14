ca6263c769301ad59fe99cb11b142b7533f73995
dmurooka <dmurooka@gmail.com>
2023-05-14 10:44:46 +0900

    Learned eventArg

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function () {
-    console.log('Lister called');
+emitter.on('messageLogged', (arg) => {
+    console.log('Lister called', arg);
 });
 
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', { id: 1, url: 'http://' });
 
