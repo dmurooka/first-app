77e105da8b85ab70cae05ee4848252e0cd9623b4
dmurooka <dmurooka@gmail.com>
2023-05-15 09:36:06 +0900

    Extending EventEmitter

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
-emitter.on('messageLogged', (arg) => {
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+logger.on('messageLogged', (arg) => {
     console.log('Lister called', arg);
 });
 
-emitter.emit('messageLogged', { id: 1, url: 'http://' });
-
+logger.log('message');
